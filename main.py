5a12c0576c19181853a1c6827efaac187e542063
MateuszKretkowski <mkretkowski13@gmail.com>
2024-11-21 21:39:28 +0100

    Added CountColors function, Added a testing map.

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
 from PIL import Image
-from collections import defaultdict
+
+def CountColors(imagePath):
+    image = Image.open(imagePath)
+    colors = {
+        "rgb": 0,
+        }
+    for pixelX in range(image.width):
+        for pixelY in range(image.height):
+            color = image.getpixel((pixelX, pixelY))
+            if (len(color) == 4):
+                r, g, b, a = color
+                alpha = a
+            if color in colors and alpha > 0:
+                print("There is a duplicate color: ", color, " at pixel: ", pixelX, " : ", pixelY)
+                return
+            else:
+                colors[color] = 1
+    print("There is NONE duplicate pixels")
+    return
+
+
 
 def main():
-    print("Hello World!")
+    CountColors("./TestImages/Player.map.png")
 
 if __name__ == "__main__":
     main()
