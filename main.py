e54b22fbe3c3882f1f7ad52b5963050578c0a58f
MateuszKretkowski <mkretkowski13@gmail.com>
2024-11-21 22:12:18 +0100

    Renamed variables, added more clarity to this code

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
 from PIL import Image
 
-def CountColors(imagePath):
-    image = Image.open(imagePath)
+def CountColors(texturePath):
+    texture = Image.open(texturePath)
     colors = {
         "rgb": 0,
         }
-    for pixelX in range(image.width):
-        for pixelY in range(image.height):
-            color = image.getpixel((pixelX, pixelY))
+    for pixelX in range(texture.width):
+        for pixelY in range(texture.height):
+            color = texture.getpixel((pixelX, pixelY))
             if (len(color) == 4):
                 r, g, b, a = color
                 alpha = a
             if color in colors and alpha > 0:
-                print("There is a duplicate color: ", color, " at pixel: ", pixelX, " : ", pixelY)
+                print("There is one, or more duplicate colors: ", color, " at pixel: ", pixelX, " : ", pixelY)
                 return
             else:
-                colors[color] = 1
+                colors[color] = (pixelX, pixelY)
     print("There is NONE duplicate pixels")
     return
 
 
-
-def main():
+def main(): 
     CountColors("./TestImages/Player.map.png")
 
 if __name__ == "__main__":
