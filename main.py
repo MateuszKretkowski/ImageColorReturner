92f6d4da1dd20d6cf54434bb0e3f9dee9695b541
MateuszKretkowski <mkretkowski13@gmail.com>
2024-11-23 12:10:54 +0100

    Added ColorTexture(): colors a perfect {obj}.map from a given map. Added comments

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from PIL import Image
+import random
 
 def CountColors(texturePath):
     texture = Image.open(texturePath)
@@ -19,9 +20,34 @@ def CountColors(texturePath):
     print("There is NONE duplicate pixels")
     return
 
+def ColorTexture(texturePath, width, height, outputPath):
+    texture = Image.open(texturePath)
+    image = Image.new("RGBA", (width, height), (0, 0, 0, 0))
+    imagePixels = image.load()
+
+    newColorsList = []
+    for pixelX in range(texture.width):
+        for pixelY in range(texture.height):
+            color = texture.getpixel((pixelX, pixelY))
+            r, g, b, a = color
+            alpha = a
+            if alpha > 0:
+                randomColor = (random.randrange(0, 255), random.randrange(0, 255), random.randrange(0, 255), 255)
+                if randomColor in newColorsList:
+                    print("There is a duplicate in the reworked Texture")
+                else:
+                    imagePixels[pixelX, pixelY] = randomColor
+                newColorsList.append(randomColor)
+    image.save(outputPath)
+
+
 
 def main(): 
     CountColors("./TestImages/Player.map.png")
+
+    # These are my presets for the map coloring. use the same parameters as your {obj}.map.png .
+    # If you don't have a map, you can even color the silhouette with one color. this Funcion will cover the rest.
+    ColorTexture("./TestImages/Player.map.png", 64, 80, "./TestImages/Player_reworked.map.png")
 
 if __name__ == "__main__":
     main()
