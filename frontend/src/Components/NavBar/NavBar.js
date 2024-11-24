5f9b90177972178a0629026b9d964e9fdafef46c
MateuszKretkowski <mkretkowski13@gmail.com>
2024-11-24 18:00:56 +0100

    finished navbar.

--- frontend/src/Components/NavBar/NavBar.js
+++ frontend/src/Components/NavBar/NavBar.js
@@ -1,9 +1,14 @@
+import { useNavigate } from "react-router-dom"
 import "./navbar.css"
+
 export const NavBar = () => {
-    
+    const nav = useNavigate();
     return (
         <div className="Navbar">
         <div className='navbar_container'>
+            <div className="button_list">
+                <button>Color Duplicate Picker</button>
+            </div>
             <div className='title_container'>
                 <div className='title-left-side'>
                     <div className="title_left-side_word_big">
@@ -18,6 +23,9 @@ export const NavBar = () => {
                 <div className="title_right-side_word">
                     <h1>ASSISTANT</h1>
                 </div>
+            </div>
+            <div className="button_list">
+                <button>Color Painter Assistant</button>
             </div>
         </div>
         </div>
