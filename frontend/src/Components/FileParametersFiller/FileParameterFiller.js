bc2d87d571c1ea21a37857a9867c777ee1dcb072
Mateusz Kretkowski <ati013@wp.pl>
2024-12-01 01:27:38 +0100

    redid some things

--- frontend/src/Components/FileParametersFiller/FileParameterFiller.js
+++ frontend/src/Components/FileParametersFiller/FileParameterFiller.js
@@ -1,12 +1,7 @@
 import { useEffect, useState } from "react";
 
-export const FileParameterFiller = ({ data, setData, stringArray }) => {
+export const FileParameterFiller = ({ data, setData, stringArray, lastIndex }) => {
     const [isToggled, setIsToggled] = useState(false);
-
-    useEffect(() => {
-        console.log("Data:", data);
-        console.log("StringArray:", stringArray);
-    }, [data, stringArray]);
 
     useEffect(() => {
         const interval = setInterval(() => {
@@ -25,15 +20,29 @@ export const FileParameterFiller = ({ data, setData, stringArray }) => {
 
     return (
         <div className="fpf">
+            <span>const data = &#123;</span> {/* Wizualne {} */}
             {stringArray?.map((key, index) => (
-                <pre key={index}>
-                    {key}: <input
-                        type="text"
-                        value={data[key] || ""}
-                        onChange={(e) => handleInputChange(key, e.target.value)}
-                    />
+                <pre className="fpf_pre" key={index}>
+                    {/* Jeśli to nie ostatni element, renderujemy input */}
+                    {index !== stringArray.length - 1 && (
+                        <>
+                            {key}: <input
+                                type="text"
+                                value={data[key] || ""}
+                                onChange={(e) => handleInputChange(key, e.target.value)}
+                            />
+                        </>
+                    )}
+    
+                    {/* Jeśli to ostatni element, renderujemy przycisk */}
+                    {index === stringArray.length - 1 && (
+                        <>
+                        {key}: <button className="small_btn">Add New Object</button>
+                        </>
+                    )}
                 </pre>
             ))}
+            <span>&#125;</span> {/* Wizualne zamknięcie nawiasu */}
             {isToggled ? "_" : ""}
         </div>
     );
